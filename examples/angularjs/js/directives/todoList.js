de4432e6711ee254832fc5e1d5fc795d728ab01b
brendonco <brendonco@hotmail.com>
2015-07-30 13:09:43 +0800

    Update todoList.js

--- examples/angularjs/js/directives/todoList.js
+++ examples/angularjs/js/directives/todoList.js
@@ -5,9 +5,6 @@ angular.module('todomvc')
         return {
                 restrict: 'EA',
                 scope: true,
-                templateUrl: 'todoList.html'  
-
-                // The html tags are declared at index.html with ng-template
-                // Took me awhile to know how to use it.
+                templateUrl: 'todoList.html'
         };
     });
